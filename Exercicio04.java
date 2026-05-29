2f6aa5db2155c6f3467710ea6d4ad89a4c4a911f
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-05-28 21:06:26 -0300

    Criacao do arquivo java exercicio 04

---
+++ Exercicio04.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        scn.close();
+    }
+}
