088c62fb8cd5f370cdf308a9d3e4a3d1f5200e79
Manoel Amancio <manoel.amanciio@gmail.com>
2026-05-28 21:11:43 -0300

    Criado laço de repetição

---
+++ Exercicio9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = 6;
+        System.out.println("6 é divisivel por " + numero + ": "); 
+
+        for (int i = 1; i < numero; i++) {
+            if (numero % i == 0) {
+                System.out.println(i);
+            }
+            
+        }
+
+        sc.close();
+    }
+}
